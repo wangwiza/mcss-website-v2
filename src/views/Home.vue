af761eb66d8de157242a97c85d9ed7fff574de06
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-05-03 17:45:45 -0400

    (fix): removed tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,5 +8,8 @@ const route = useRoute()
 </script>
 
 <template>
+    <h1 class="page-title">
+        hello world!
+    </h1>
     <router-link to='/about'>Click this button to go to about</router-link>
 </template>
