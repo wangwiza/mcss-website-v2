c1f50920b437c8a3f61a20af5b5587e3f4080764
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-07-13 23:04:55 -0400

    (fix): images now hosted online by Cloudinary

--- src/slides.js
+++ src/slides.js
@@ -3,60 +3,33 @@ export const web_slides = [
         {
             title: 'Spring Waltz',
             content: 'Annual semi-formal at Plaza Centre-Ville',
-            image: '/assets/images/F22/spring_waltz_banner.png'
+            image: 'https://res.cloudinary.com/mcss-website/image/upload/v1689123119/mcss/events/2022-2023/spring_waltz_banner.png'
         },
         {
             title: "Are You Game?",
             content: "Game night at Gert's",
-            image: '/assets/images/F22/fyrs_banner.png'
+            image: 'https://res.cloudinary.com/mcss-website/image/upload/v1689123119/mcss/events/2022-2023/fyrs_banner.png'
         },
         {
             title: "Find Your Lucky Charm",
             content: "Mingling at Apt 200",
-            image: '/assets/images/F22/lucky_charm_banner.png'
+            image: 'https://res.cloudinary.com/mcss-website/image/upload/v1689123119/mcss/events/2022-2023/lucky_charm_banner.png'
         },
         {
             title: "Lunar New Year 2023",
             content: "Annual CNY fair at SSMU",
-            image: '/assets/images/F22/lny_2023_banner.png'
+            image: 'https://res.cloudinary.com/mcss-website/image/upload/v1689123119/mcss/events/2022-2023/lny_2023_banner.png'
         },
         {
             title: "Casino Night",
             content: "Gambling at Place des Arts",
-            image: '/assets/images/F22/casino_night_banner.png'
+            image: 'https://res.cloudinary.com/mcss-website/image/upload/v1689123119/mcss/events/2022-2023/casino_night_banner.png'
         },
         {
             title: "Night Market",
             content: "Welcoming Party at SSMU",
-            image: '/assets/images/F22/night_market_banner.png'
+            image: 'https://res.cloudinary.com/mcss-website/image/upload/v1689123120/mcss/events/2022-2023/night_market_banner.png'
         },
     ],
     // Old Slides
-    [
-        {
-            title: 'Spring Waltz',
-            content: 'Annual semi-formal at Plaza Centre-Ville',
-            image: '/assets/images/F22/spring_waltz_banner.png'
-        },
-        {
-            title: "Are You Game?",
-            content: "Game night at Gert's",
-            image: '/assets/images/F22/fyrs_banner.png'
-        },
-        {
-            title: "Find Your Lucky Charm",
-            content: "Mingling at Apt 200",
-            image: '/assets/images/F22/lucky_charm_banner.png'
-        },
-        {
-            title: "Lunar New Year 2023",
-            content: "Annual CNY fair at SSMU",
-            image: '/assets/images/F22/lny_2023_banner.png'
-        },
-        {
-            title: "Casino Night",
-            content: "Gambling at Place des Arts",
-            image: '/assets/images/F22/casino_night_banner.png'
-        }
-    ]
 ]
