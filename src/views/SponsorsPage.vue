42c6360768198545305f23a5f89277a0dd0cb33d
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-07-24 22:27:28 -0400

    (feat): sponsor membership card section

--- src/views/SponsorsPage.vue
+++ src/views/SponsorsPage.vue
@@ -1,19 +1,81 @@
 <script setup>
 import { sponsors } from '@/data/sponsors.js';
+import { VueperSlides, VueperSlide } from 'vueperslides'
 import CloudImage from '@/components/CloudImage.vue';
+import 'vueperslides/dist/vueperslides.css'
+import MCard from "vue-3dcss-card";
 </script>
 
 <template>
-    <h1>Sponsors</h1>
-    <div class="sponsor-grid">
-        <div class="sponsor-grid__item" v-for="sponsor in sponsors" :key="sponsor">
-            <CloudImage :imageName="'mcss/sponsors/' + sponsor.image" />
+    <div class="wrapper">
+
+        <h1 class="page-title">MCSS 2023-2024 MEMBERSHIP CARD</h1>
+        <p class="p1">
+
+            <CloudImage class="membership-card" imageName="mcss/card/front" />
+        </p>
+        <p class="p1">
+            MTSA, HKSN, MCSS and MECA have partnered together to bring you another joint
+            Membership Card!
+
+            With our membership card, students can get numerous discounts at various restaurants and stores across the city.
+            We
+            take pride in bringing our members the best deals and benefits with our annually updated list of participating
+            sponsors.
+        </p>
+        <p class="p1">&nbsp</p>
+        <p class="p1">&nbsp</p>
+        <p class="p1">&nbsp</p>
+
+        <h1 class="page-title">OUR SPONSORS</h1>
+        <vueper-slides class="no-shadow" :visible-slides="5" :arrows="false" :slide-ratio="1 / 8" :bullets="false" autoplay
+            duration="1000" :gap="3" :dragging-distance="70">
+            <vueper-slide v-for="sponsor in sponsors" :key="sponsor" :title="sponsor.name" :image="sponsor.image" />
+        </vueper-slides>
+
+        <div class="sponsor-grid">
+            <div class="sponsor-grid__item" v-for="sponsor in sponsors" :key="sponsor">
+                <CloudImage :imageName="'mcss/sponsors/' + sponsor.image" />
+            </div>
         </div>
-    </div> 
-    
+    </div>
 </template>
 
 <style scoped>
+.wrapper {
+    justify-content: center;
+    margin-bottom: 30px;
+
+    position: relative;
+    max-width: 1660px;
+    width: 66.666%;
+    left: 16.666%
+}
+
+.page-title {
+    font-size: var(--header-text-size-px);
+    width: 100%;
+    text-align: center;
+    display: inline-block;
+    position: relative;
+    margin-bottom: 45px;
+    letter-spacing: var(--heading-spacing);
+    text-transform: var(--heading-font-case);
+}
+
+.p1 {
+  text-align: center;
+  margin-bottom: 15px;
+  
+}
+
+.membership-card {
+    width: 30vw;
+    height: auto;
+    border-radius: 1vw;
+    box-shadow: 10px 10px;
+}
+
 .sponsor-grid {
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
@@ -30,5 +92,4 @@ img {
     height: auto;
     margin: auto;
 }
-
 </style>
