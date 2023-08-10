0c5486dc66de5a10e39c6a02cb055b715f466adc
wangwiza <76924804+wangwiza@users.noreply.github.com>
2023-08-09 21:42:34 -0400

    (feat): the chicken

--- src/components/Scene.vue
+++ src/components/Scene.vue
@@ -1,35 +1,70 @@
 <script setup>
 import * as THREE from 'three';
+
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
+
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
 import { ref, onMounted } from 'vue';
 
 onMounted(() => {
-    const container = document.getElementById('container')
-    console.log(container)
-    
-    const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    
-    const renderer = new THREE.WebGLRenderer();
+    let mixer;
+
+    const clock = new THREE.Clock();
+    const container = document.getElementById('container');
+
+    const renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     container.appendChild(renderer.domElement);
-    
-    const geometry = new THREE.BoxGeometry(1, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    
-    camera.position.z = 5;
-    
+
+    const pmremGenerator = new THREE.PMREMGenerator(renderer);
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xffffff);
+    scene.environment = pmremGenerator.fromScene(new RoomEnvironment(renderer), 0.04).texture;
+
+    const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 100);
+    camera.position.set(5, 3, 8);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.target.set(0, 0.5, 0);
+    controls.autoRotate = true
+    controls.autoRotateSpeed = 20
+    controls.enablePan = false
+    controls.enableZoom = false
+
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath('jsm/libs/draco/gltf/');
+
+    const loader = new GLTFLoader();
+    loader.setDRACOLoader(dracoLoader);
+    loader.load('/assets/models/chicken.glb', function (gltf) {
+        const model = gltf.scene;
+        model.position.set(0, 0, 0);
+        model.scale.set(0.01, 0.01, 0.01);
+        scene.add(model);
+        mixer = new THREE.AnimationMixer(model);
+        mixer.clipAction(gltf.animations[0]).play();
+        animate();
+    }, undefined, function (e) {
+        console.error(e);
+    });
+
+    window.onresize = function () {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    };
+
     function animate() {
         requestAnimationFrame(animate);
-        
-        cube.rotation.x += 0.01;
-        cube.rotation.y += 0.01;
-        
+        const delta = clock.getDelta();
+        mixer.update(delta);
+        controls.update();
         renderer.render(scene, camera);
     }
-    
-    animate();
 })
 </script>
 
@@ -37,5 +72,4 @@ onMounted(() => {
     <div id="container"></div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -3,6 +3,5 @@ import Scene from "@/components/Scene.vue"
 </script>
 
 <template>
-  <h1>hello world!</h1>
   <Scene />
 </template>
